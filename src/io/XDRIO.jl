b0048e1b2e1500c53262623380e4c26ff27d6fe3
Alexey Stukalov <astukalov@gmail.com>
2017-03-09 17:34:27 +0100

    remove ctor @compat for v0.4

--- src/io/XDRIO.jl
+++ src/io/XDRIO.jl
@@ -4,7 +4,7 @@
 type XDRIO{T<:IO} <: RDAIO
     sub::T             # underlying IO stream
     buf::Vector{UInt8} # buffer for strings
-    @compat (::Type{XDRIO}){T <: IO}(io::T) = new{T}(io, Vector{UInt8}(1024))
+    (::Type{XDRIO}){T <: IO}(io::T) = new{T}(io, Vector{UInt8}(1024))
 end
 
 readint32(io::XDRIO) = ntoh(read(io.sub, Int32))
