d4cbe726ed9982658f9a5ff253ea52ca3a69f464
Douglas Bates <dmbates@gmail.com>
2017-03-31 16:01:21 -0500

    Incorporate suggestions by @ararslan

--- src/context.jl
+++ src/context.jl
@@ -8,7 +8,7 @@ type RDAContext{T<:RDAIO}
     io::T                      # RDA input stream
 
     # RDA format properties
-    fmtver::Int32             # format version
+    fmtver::UInt32             # format version
     Rver::VersionNumber        # R version used to write the file
     Rmin::VersionNumber        # minimal R version to read the file
 
@@ -17,17 +17,13 @@ type RDAContext{T<:RDAIO}
     # intermediate data
     ref_tab::Vector{RSEXPREC}  # SEXP array for references
 end
+int2ver(v::Integer) = VersionNumber(v >> 16, (v >> 8) & 0xff, v & 0xff)
 function RDAContext{T<:RDAIO}(io::T, kwoptions::Vector{Any}=Any[])
-  fmtver = readint32(io)
-  rver = readint32(io)
-  rminver = readint32(io)
-  kwdict = Dict{Symbol,Any}(kwoptions)
-  RDAContext(io,
-      fmtver,
-      VersionNumber(div(rver,65536), div(rver%65536, 256), rver%256),
-      VersionNumber(div(rminver,65536), div(rminver%65536, 256), rminver%256),
-      kwdict,
-      RSEXPREC[])
+    fmtver = readuint32(io)
+    rver = int2ver(readint32(io))
+    rminver = int2ver(readint32(io))
+    kwdict = Dict{Symbol,Any}(kwoptions)
+    RDAContext(io, fmtver, rver, rminver, kwdict, RSEXPREC[])
 end
 
 """
