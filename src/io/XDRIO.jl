4a7bebbfac94e185c57ce59830a59bd835a0073e
Douglas Bates <dmbates@gmail.com>
2017-03-31 14:37:57 -0500

    Use comprehensions

--- src/io/XDRIO.jl
+++ src/io/XDRIO.jl
@@ -9,22 +9,16 @@ end
 
 readint32(io::XDRIO) = ntoh(read(io.sub, Int32))
 readuint32(io::XDRIO) = ntoh(read(io.sub, UInt32))
-readfloat64(io::XDRIO) = ntoh(read(io.sub, Float64))
+readfloat64(io::XDRIO) = reinterpret(Float64, ntoh(read(io.sub, Int64)))
 
 readintorNA(io::XDRIO) = readint32(io)
-function readintorNA(io::XDRIO, n::RVecLength)
-    v = read(io.sub, Int32, n)
-    map!(ntoh, v, v)
-end
+readintorNA(io::XDRIO, n::RVecLength) = [readint32(io) for _ in 1:n]
 
 # this method have Win32 ABI issues, see JuliaStats/RData.jl#5
 # R's NA is silently converted to NaN when the value is loaded in the register(?)
 #readfloatorNA(io::XDRIO) = readfloat64(io)
 
-function readfloatorNA(io::XDRIO, n::RVecLength)
-    v = read(io.sub, UInt64, n)
-    reinterpret(Float64, map!(ntoh, v, v))
-end
+readfloatorNA(io::XDRIO, n::RVecLength) = [readfloat64(io) for _ in 1:n]
 
 readuint8(io::XDRIO, n::RVecLength) = readbytes(io.sub, n)
 
