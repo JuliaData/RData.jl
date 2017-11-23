45478346ee625f111cb6579f37a1b410409005a0
James Sams <sams.james@gmail.com>
2017-11-23 09:29:39 -0800

    add test for isa DataFrame for rds files

--- test/RDS.jl
+++ test/RDS.jl
@@ -13,22 +13,32 @@ module TestRDS
                        factor = pool(["ab", "c"]),
                        cplx = Complex128[1.1+0.5im, 1.0im])
         rdf = sexp2julia(load("$testdir/data/types.rds",convert=false))
+        @test rdf isa DataFrame
         @test eltypes(rdf) == eltypes(df)
         @test isequal(rdf, df)
+
         rdf_ascii = sexp2julia(load("$testdir/data/types_ascii.rds",convert=false))
+        @test rdf_ascii isa DataFrame
         @test eltypes(rdf_ascii) == eltypes(df)
         @test isequal(rdf_ascii, df)
+
         rdf_decomp = sexp2julia(load("$testdir/data/types_decomp.rds",convert=false))
+        @test rdf_decomp isa DataFrame
         @test eltypes(rdf_decomp) == eltypes(df)
         @test isequal(rdf_decomp, df)
 
         rdf = load("$testdir/data/types.rds")
+        @test rdf isa DataFrame
         @test eltypes(rdf) == eltypes(df)
         @test isequal(rdf, df)
+
         rdf_ascii = load("$testdir/data/types_ascii.rds")
+        @test rdf_ascii isa DataFrame
         @test eltypes(rdf_ascii) == eltypes(df)
         @test isequal(rdf_ascii, df)
+
         rdf_decomp = load("$testdir/data/types_decomp.rds")
+        @test rdf_decomp isa DataFrame
         @test eltypes(rdf_decomp) == eltypes(df)
         @test isequal(rdf_decomp, df)
     end
