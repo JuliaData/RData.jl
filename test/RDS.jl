a5bb2b58ed6d22faaa5f84dcd2d40d2072f20011
Alexey Stukalov <alyst@users.noreply.github.com>
2018-03-14 22:46:52 +0100

    whitespace fix and test name enh

--- test/RDS.jl
+++ test/RDS.jl
@@ -5,24 +5,24 @@ module TestRDS
 
     testdir = dirname(@__FILE__)
 
-    @testset "RDS: Conversion to Julia types" begin
+    @testset "Loading RDS files" begin
         df = DataFrame(num = [1.1, 2.2],
                        int = Int32[1, 2],
                        logi = [true, false],
                        chr = ["ab", "c"],
                        factor = categorical(["ab", "c"], true),
                        cplx = Complex128[1.1+0.5im, 1.0im])
-        rdf = sexp2julia(load("$testdir/data/types.rds",convert=false))
+        rdf = sexp2julia(load("$testdir/data/types.rds", convert=false))
         @test rdf isa DataFrame
         @test eltypes(rdf) == eltypes(df)
         @test isequal(rdf, df)
 
-        rdf_ascii = sexp2julia(load("$testdir/data/types_ascii.rds",convert=false))
+        rdf_ascii = sexp2julia(load("$testdir/data/types_ascii.rds", convert=false))
         @test rdf_ascii isa DataFrame
         @test eltypes(rdf_ascii) == eltypes(df)
         @test isequal(rdf_ascii, df)
 
-        rdf_decomp = sexp2julia(load("$testdir/data/types_decomp.rds",convert=false))
+        rdf_decomp = sexp2julia(load("$testdir/data/types_decomp.rds", convert=false))
         @test rdf_decomp isa DataFrame
         @test eltypes(rdf_decomp) == eltypes(df)
         @test isequal(rdf_decomp, df)
