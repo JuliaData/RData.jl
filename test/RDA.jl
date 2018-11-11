0e29338004793db991794110271ddcf574612540
Alexey Stukalov <astukalov@gmail.com>
2018-11-11 01:44:27 +0100

    kwarg whitespace fixes

--- test/RDA.jl
+++ test/RDA.jl
@@ -18,8 +18,8 @@ module TestRDA
     testdir = dirname(@__FILE__)
     @testset "Reading minimal RData" begin
         df = DataFrame(num = [1.1, 2.2])
-        @test sexp2julia(load("$testdir/data/minimal.rda",convert=false)["df"]) == df
-        @test load("$testdir/data/minimal.rda",convert=true)["df"] == df
+        @test sexp2julia(load("$testdir/data/minimal.rda", convert=false)["df"]) == df
+        @test load("$testdir/data/minimal.rda", convert=true)["df"] == df
         @test load("$testdir/data/minimal_ascii.rda")["df"] == df
     end
 
@@ -30,10 +30,10 @@ module TestRDA
                        chr = ["ab", "c"],
                        factor = categorical(["ab", "c"], true),
                        cplx = [1.1+0.5im, 1.0im])
-        rdf = sexp2julia(load("$testdir/data/types.rda",convert=false)["df"])
+        rdf = sexp2julia(load("$testdir/data/types.rda", convert=false)["df"])
         @test eltypes(rdf) == eltypes(df)
         @test rdf == df
-        rdf_ascii = sexp2julia(load("$testdir/data/types_ascii.rda",convert=false)["df"])
+        rdf_ascii = sexp2julia(load("$testdir/data/types_ascii.rda", convert=false)["df"])
         @test eltypes(rdf_ascii) == eltypes(df)
         @test rdf_ascii == df
     end
@@ -50,26 +50,26 @@ module TestRDA
         append!(df, df[2, :])
         df[3, :num] = NaN
         df[:, :cplx] = [missing, ComplexF64(1,NaN), NaN]
-        @test isequal(sexp2julia(load("$testdir/data/NAs.rda",convert=false)["df"]), df)
+        @test isequal(sexp2julia(load("$testdir/data/NAs.rda", convert=false)["df"]), df)
         # ASCII format saves NaN as NA
         df[3, :num] = missing
         df[:, :cplx] = missing
-        @test isequal(sexp2julia(load("$testdir/data/NAs_ascii.rda",convert=false)["df"]), df)
+        @test isequal(sexp2julia(load("$testdir/data/NAs_ascii.rda", convert=false)["df"]), df)
     end
 
     @testset "Column names conversion" begin
-        rda_names = names(sexp2julia(load("$testdir/data/names.rda",convert=false)["df"]))
+        rda_names = names(sexp2julia(load("$testdir/data/names.rda", convert=false)["df"]))
         expected_names = [:_end, :x!, :x1, :_B_C_, :x, :x_1]
         @test rda_names == expected_names
-        rda_names = names(sexp2julia(load("$testdir/data/names_ascii.rda",convert=false)["df"]))
+        rda_names = names(sexp2julia(load("$testdir/data/names_ascii.rda", convert=false)["df"]))
         @test rda_names == [:_end, :x!, :x1, :_B_C_, :x, :x_1]
     end
 
     @testset "Reading RDA with complex types (environments, closures etc)" begin
-        rda_envs = load("$testdir/data/envs.rda",convert=false)
-        rda_pairlists = load("$testdir/data/pairlists.rda",convert=false)
-        rda_closures = load("$testdir/data/closures.rda",convert=false)
-        rda_cmpfuns = load("$testdir/data/cmpfun.rda",convert=false)
+        rda_envs = load("$testdir/data/envs.rda", convert=false)
+        rda_pairlists = load("$testdir/data/pairlists.rda", convert=false)
+        rda_closures = load("$testdir/data/closures.rda", convert=false)
+        rda_cmpfuns = load("$testdir/data/cmpfun.rda", convert=false)
     end
 
     @testset "Proper handling of factor and ordered" begin
