915f879dc78c889af876a4fee01b7c003e1ce6ad
Whit Armstrong <armstrong.whit@gmail.com>
2018-11-16 09:42:57 -0500

    regenerate R datafiles each time test suite is run

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using RData
 using Test
 
+generate_data_rscript = joinpath(dirname(@__FILE__),"generate_rda.R")
+run(Cmd(["Rscript","--default-packages=methods,compiler",generate_data_rscript]))
 include("RDA.jl")
 include("RDS.jl")
