6b4a934db0808044d528cfc89d329d28ccab8f79
Alexey Stukalov <astukalov@gmail.com>
2019-06-18 11:02:53 +0200

    unescape_rstring(): unescape '

--- test/basic.jl
+++ test/basic.jl
@@ -16,6 +16,11 @@ using RData
     @test RData.isna_float64(reinterpret(UInt64, RData.R_NA_FLOAT64 | ((Base.significand_mask(Float64) + 1) >> 1)))
 end
 
+@testset "Unescape R strings" begin
+    @test RData.unescape_rstring("\\\"") == "\""
+    @test RData.unescape_rstring("\\'") == "'"
+end
+
 end
 
 end # TestBasic
