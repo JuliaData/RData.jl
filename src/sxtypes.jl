e1700286b4007a6af81c909dc2123ab94851b1e3
Alexey Stukalov <astukalov@gmail.com>
2020-06-24 14:21:50 +0200

    add unwrap(::RAltRep) and getdata(:RAltRep)

--- src/sxtypes.jl
+++ src/sxtypes.jl
@@ -280,3 +280,38 @@ Base.size(rv::RVEC) = length(rv.data)
 Base.size(rl::RList) = isdataframe(rl) ? (length(rl.data[1]), length(rl.data)) : length(rl.data)
 
 row_names(ro::ROBJ) = getattr(ro, "row.names", emptystrvec)
+
+# unwrap data contained in RAltRep
+function unwrap(ar::RAltRep)
+    if isa(ar.info, RPairList) && length(ar.info) >= 1 &&
+        isa(ar.info.items[1], RSymbol) && startswith(string(ar.info.items[1]), "wrap_")
+        # the first element of the AltRep state should be the wrapped one
+        if isa(ar.state, RPairList) && length(ar.state) >= 1
+            data = ar.state.items[1] # the actual data
+            # recover object attributes from the AltRep head
+            for (attrname, attr) in ar.attr
+                if !haskey(data.attr, attrname)
+                    if data.attr === emptyhash
+                        # make sure data has its own dedicated attribute storage
+                        data = addattr(data)
+                    end
+                    data.attr[attrname] = attr
+                end
+            end
+            return data
+        else
+            error("Unexpected state of \"$(ar.info.items[1])\" AltRep SEXP")
+        end
+    else
+        # TODO support compact_intseq and compact_realseq AltRep
+        @warn "Unsupported AltRep SEXP"
+        return nothing
+    end
+end
+
+# accessing AltRep data is special
+function getdata(ar::RAltRep)
+    unwrapped = unwrap(ar)
+    return (unwrapped === nothing) || (unwrapped === ar) ? unwrapped : getdata(unwrapped)
+end
+
